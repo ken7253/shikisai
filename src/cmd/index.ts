2e8e746c377872ee92ae1c9b203d3c84deda787d
ken7253 <ken7253@gmail.com>
2021-05-02 02:33:12 +0900

    create init function

--- src/cmd/index.ts
+++ src/cmd/index.ts
@@ -1,6 +1,19 @@
+import fs = require('fs');
+
+const root = process.argv[1];
+
 export default {
   init(name: string) {
-    console.log(`project name is ${name}`);
+    const paletteTemplateDir = `${root}/static/colorpalette.config.json`;
+    const settingTemplate = fs.readFileSync(paletteTemplateDir, {
+      encoding: 'utf-8',
+    });
+    const settingJson = JSON.parse(settingTemplate);
+    settingJson.projectName = name;
+    fs.writeFileSync(
+      'colorpalette.config.json',
+      JSON.stringify(settingJson, null, 2)
+    );
   },
   add(colorName: string, colorCode: string) {
     console.log(colorName, colorCode);
