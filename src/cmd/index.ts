d08980e2097809670a368e7a52902f922e8bde00
ken7253 <ken7253@gmail.com>
2021-05-03 01:02:57 +0900

    create replacePalette function

--- src/cmd/index.ts
+++ src/cmd/index.ts
@@ -2,6 +2,27 @@ import * as fs from 'fs';
 
 import common from '../modules/globals';
 
+/**
+ * replace to colorpalette.config.json
+ *
+ * @param targetKey string ka
+ * @param newValue unknown
+ */
+
+const replacePalette = (targetKey: string[], newValue: unknown) => {
+  const paletteTemplateDir = `${common.root}/colorpalette.config.json`;
+  const palette = fs.readFileSync(paletteTemplateDir, {
+    encoding: 'utf-8',
+  });
+  const json = JSON.parse(palette);
+  console.log(json);
+  if (newValue !== null) {
+    // json からtargetKeyで指定されたKeyを取得してvalueをnewValueに変更する処理
+  } else {
+    // newValueがnullの場合targetKeyで指定された値を削除する処理
+  }
+};
+
 export default {
   init(name: string) {
     const paletteTemplateDir = `${common.jsonFiles}/colorpalette.config.json`;
@@ -15,9 +36,11 @@ export default {
       JSON.stringify(settingJson, null, 2)
     );
   },
+
   add(colorName: string, colorCode: string) {
     console.log(colorName, colorCode);
   },
+
   remove(colorName: string) {
     console.log(colorName);
   },
