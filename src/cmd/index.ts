53961a809d8cf484d27c41f9cb7ebb848f08e9ca
ken7253 <ken7253@gmail.com>
2021-05-04 03:56:45 +0900

    create add cmd *Not tested yet*

--- src/cmd/index.ts
+++ src/cmd/index.ts
@@ -2,30 +2,41 @@ import * as fs from 'fs';
 
 import common from '../modules/globals';
 
-/**
- * replace to colorpalette.config.json
- *
- * @param targetKey string ka
- * @param newValue unknown
- */
+interface colorUnit {
+  name: string;
+  data: {
+    hex: string;
+    rgb: [number, number, number];
+    hls: [number, number, number];
+  };
+}
 
-const replacePalette = (targetKey: string[], newValue: unknown) => {
-  const paletteTemplateDir = `${common.root}/colorpalette.config.json`;
-  const palette = fs.readFileSync(paletteTemplateDir, {
-    encoding: 'utf-8',
-  });
-  const json = JSON.parse(palette);
-  console.log(json);
-  if (newValue !== null) {
-    // json からtargetKeyで指定されたKeyを取得してvalueをnewValueに変更する処理
-  } else {
-    // newValueがnullの場合targetKeyで指定された値を削除する処理
-  }
+interface Palette {
+  name: string;
+  dist: string;
+  compileType: 'css' | 'scss';
+  color: colorUnit[];
+}
+
+const editConfig = (func: Function) => {
+  const getPalette = fs.readFileSync(
+    `${common.root}/colorpalette.config.json`,
+    {
+      encoding: 'utf-8',
+    }
+  );
+  const palette: Palette = JSON.parse(getPalette);
+  const afterEditPalette = func(palette);
+
+  fs.writeFileSync(
+    common.CONFIG_FILE_NAME,
+    JSON.stringify(afterEditPalette, null, 2)
+  );
 };
 
 export default {
   init(name: string) {
-    const paletteTemplateDir = `${common.jsonFiles}/colorpalette.config.json`;
+    const paletteTemplateDir = `${common.JSON_FILE_DIRECTORY}/colorpalette.config.json`;
     const settingTemplate = fs.readFileSync(paletteTemplateDir, {
       encoding: 'utf-8',
     });
@@ -38,7 +49,29 @@ export default {
   },
 
   add(colorName: string, colorCode: string) {
-    console.log(colorName, colorCode);
+    editConfig((palette: Palette) => {
+      // 既にcolorNameと同じ名前が使用されていないか確認
+      try {
+        palette.color.forEach(obj => {
+          if (obj.name === colorName) {
+            throw `${colorName} has already been used.`;
+          }
+        });
+      } catch (error) {
+        console.error(error);
+      }
+      // colorNameの重複がない場合
+      const newColorUnit: colorUnit = {
+        name: colorName,
+        data: {
+          hex: colorCode,
+          rgb: [0, 0, 0],
+          hls: [0, 0, 0],
+        },
+      };
+      palette.color.push(newColorUnit);
+      return palette;
+    });
   },
 
   remove(colorName: string) {
