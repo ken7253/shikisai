a1bfc4dda383aa4c3ea34b453bda4654a3d6e5c5
ken7253 <ken7253@gmail.com>
2022-06-18 02:54:15 +0900

    サーバーの起動設定

---
+++ src/server/index.ts
@@ -0,0 +1,12 @@
+import handler from 'serve-handler';
+import http from 'http';
+
+const server = http.createServer((request, response) => {
+  return handler(request, response, {
+    public: "dist/view/"
+  });
+});
+
+server.listen(3000, () => {
+  console.log("[SERVER]: Running at http://localhost:3000");
+});
